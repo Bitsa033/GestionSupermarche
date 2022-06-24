3c561784619da99c713fbb123a3c06f31f079013
bitsapascal <bitsapascal033@gmail.com>
2022-06-25 00:08:51 +0100

    creation de l'interface permettant d'enregistrer les unités de gestion

---
+++ src/Controller/UvalController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Uval;
+use App\Form\UvalType;
+use App\Repository\UvalRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("uval_")
+ */
+class UvalController extends AbstractController
+{
+    /**
+     * @Route("index", name="uval_index", methods={"GET"})
+     */
+    public function index(UvalRepository $uvalRepository): Response
+    {
+        return $this->render('uval/index.html.twig', [
+            'uvals' => $uvalRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("new", name="uval_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $uval = new Uval();
+        $form = $this->createForm(UvalType::class, $uval);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($uval);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('uval_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('uval/new.html.twig', [
+            'uval' => $uval,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("{id}", name="uval_show", methods={"GET"})
+     */
+    public function show(Uval $uval): Response
+    {
+        return $this->render('uval/show.html.twig', [
+            'uval' => $uval,
+        ]);
+    }
+
+    /**
+     * @Route("{id}_edit", name="uval_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Uval $uval, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(UvalType::class, $uval);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('uval_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('uval/edit.html.twig', [
+            'uval' => $uval,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="uval_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Uval $uval, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$uval->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($uval);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('uval_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/UvalType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Catuval;
+use App\Entity\Uval;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UvalType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nomuval')
+            ->add('catuval',EntityType::class,[
+                'class'=> Catuval::class,
+                'choice_label'=>'nomcatuval'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Uval::class,
+        ]);
+    }
+}
