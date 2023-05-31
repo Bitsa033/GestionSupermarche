d1b79209cfccc3315f3afbe4707caf71a49a626e
Bitsapascal <bitapascal033@gmail.com>
2023-05-31 07:18:31 +0100

    *

--- src/Repository/ProduitRepository.php
+++ src/Repository/ProduitRepository.php
@@ -42,25 +42,12 @@ class ProduitRepository extends ServiceEntityRepository
     }
     */
     
-    public function qte_en_stock()
+    public function prod_qty_tot($id)
     {
-        $sql = '
-        SELECT produit.id as id, produit.nom as produit,
-        prix_vente, SUM(qte_tot_val) as qte_en_stock, nomuval as emballage 
-        FROM reception inner join achat on achat.id = reception.commande_id 
-        inner join produit on produit.id =
-        achat.produit_id inner join uval on uval.id = produit.unite_vente_id  group by produit.id
-        ;
-        ';
-        $rray=$this->db->fetch_all_command($sql);
-        return $rray;
-    }
-
-    public function bilan_reception($id)
-    {
-        $sql1='SELECT produit_id, SUM(reception.qte_tot_val) as qte_tot_val, SUM(reception.prix_tot_val)
+        $sql1='SELECT produit_id, reception.qte_tot_val as qte_tot_val, reception.prix_tot_val
         AS prix_total_val FROM `reception` INNER JOIN achat ON achat.id = reception.commande_id 
-        INNER JOIN produit ON produit.id = achat.produit_id WHERE reception.commande_id ='.$id;
+        INNER JOIN produit ON produit.id = achat.produit_id WHERE reception.id=(select reception
+        .id from reception ORDER by id DESC LIMIT 0,1 )';
         $array=$this->db->fetch_one_command($sql1);
         $produit_id = $array['produit_id'];
         $qte_tot_val = $array['qte_tot_val'];
@@ -76,14 +63,11 @@ class ProduitRepository extends ServiceEntityRepository
         return $data;
     }
 
-    public function afficher_id_produit($id)
+    public function identity_prod($id)
     {
         $sql1='SELECT produit.id as id from produit
         INNER JOIN stock on stock.produit_id=produit.id
-        WHERE stock.produit_id = (SELECT distinct produit.id FROM 
-        reception inner join achat on achat.id =
-        reception.commande_id INNER JOIN produit
-        ON produit.id = achat.produit_id WHERE achat.id='.$id.')';
+        WHERE stock.produit_id ='.$id;
         $array=$this->db->fetch_one_command($sql1);
         $produit_id =(is_bool($array) == true) ? $array :$array['id'];
 
@@ -96,8 +80,8 @@ class ProduitRepository extends ServiceEntityRepository
 
     public function nouveau_stock($id)
     {
-        $bilanp=$this->bilan_reception($id);
-        $afficherp=$this->afficher_id_produit($id);
+        $bilanp=$this->prod_qty_tot($id);
+        $afficherp=$this->identity_prod($id);
         $produit=$afficherp['produit_id'];
         $produit_id=$bilanp['produit_id'];
         $qte_tot_val=$bilanp['qte_tot_val'];
@@ -106,8 +90,8 @@ class ProduitRepository extends ServiceEntityRepository
         if ($produit) {
             # code...
             //dd('meme id: ',$produit);
-            $sql = " UPDATE `stock` SET `qte_tot`= $qte_tot_val,
-            stock.prix_total = $prix_total_val WHERE produit_id =
+            $sql = " UPDATE `stock` SET `qte_tot`= qte_tot + $qte_tot_val,
+            stock.prix_total = stock.prix_total + $prix_total_val WHERE produit_id =
             (SELECT DISTINCT produit.id from reception INNER join 
             achat ON achat.id = reception.commande_id 
             INNER JOIN produit on produit.id = achat.produit_id WHERE achat.id=".$id." )
